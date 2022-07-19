b22a40a3c5bc42b8dcd4354272aa796db366e249
Sabry Gouda <sabry.gooda88@gmail.com>
2022-07-19 23:37:15 +0200

    pro

---
+++ src/app/models/product.ts
@@ -0,0 +1,18 @@
+export class Product {
+    /**
+     *
+     */
+    constructor(
+
+        public id:number=0,
+        public name:string="",
+        public quantity:number=0,
+        public weight:number=0,
+        public orderId:number=0,
+
+          
+    ) {
+       
+
+    }
+}
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+import { ProductData } from '../models/product-data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private BASEURL: string = "https://localhost:44372/api"
+  constructor(private http:HttpClient) { }
+
+  private token = localStorage.getItem("token");
+
+  private headers = new HttpHeaders({
+    'Authorization': `Bearer ${this.token}`
+  });
+
+  private requestOptions = { headers: this.headers };
+
+  getAll(): Observable<Product[]>{
+    return this.http.get<Product[]>(`${this.BASEURL}/products`,this.requestOptions);
+  }
+
+  getById(id:number):Observable<Product>{
+    return this.http.get<Product>(`${this.BASEURL}/products/${id}`,this.requestOptions);
+  }
+
+  insert(product:ProductData){
+
+  return  this.http.post<ProductData>(`${this.BASEURL}/Branches`,product,this.requestOptions);
+  }
+
+  update(id:number,product:Product) {
+    return this.http.put<Product>(`${this.BASEURL}/Branches/${id}`,product,this.requestOptions);
+  }
+
+  delete(id:number){
+  return  this.http.delete<Product>(`${this.BASEURL}/Branches/${id}`,this.requestOptions);
+  }
+}
