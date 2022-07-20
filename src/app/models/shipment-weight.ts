3b23da74d37e7f521a9023da8ef09927a19f4c8b
Amr M. Nouh <AMR.MAMDOUH1371@ci.menofia.edu.eg>
2022-07-20 09:38:19 +0200

    last update

--- src/app/models/shipment-weight.ts
+++ src/app/models/shipment-weight.ts
@@ -1,6 +1,7 @@
 export class ShipmentWeight {
 
   constructor(
+    public id: number = 0,
     public highestWeight: number = 0,
     public cost: number = 0,
     public additionalPrice: number = 0,
